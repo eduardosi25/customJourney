d789d4c09df00c21d05feed2292d1102d0150223
eduardosi25 <eduardosi25@hotmail.com>
2022-06-16 20:26:17 -0500

    name in test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -90,18 +90,18 @@ define([
         // var postcardURLValue = $('#postcard-url').val();
         // var postcardTextValue = $('#postcard-text').val();
         console.log("payloadAntes-->",payload['arguments'].execute.inArguments);
-        console.log("payloadAntes--> con Phone",payload['arguments'].execute.inArguments[1].Phone);
-        console.log("payloadAntes--> con Nombre",payload['arguments'].execute.inArguments[2].Nombre);
+        console.log("payloadAntes--> con Phone",payload['arguments'].execute.inArguments[1].phone);
+        console.log("payloadAntes--> con Nombre",payload['arguments'].execute.inArguments[2].name);
         //payload['arguments'].execute.inArguments = [{
         //    "tokens": authTokens
         //}];
         
         payload['arguments'].execute.inArguments = [
         {
-            'Phone': "{{Contact.Attribute.test_HD.Phone}}"
+            'Phone': "{{Contact.Attribute.test_HD.phone}}"
         },
         {
-            'Nombre': "{{Contact.Attribute.test_HD.Nombre}}"
+            'Nombre': "{{Contact.Attribute.test_HD.name}}"
         }];
 
 
