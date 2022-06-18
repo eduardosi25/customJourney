9e9db88af026591460ef1be81eba65325519f64a
eduardosi25 <eduardosi25@hotmail.com>
2022-06-18 01:26:19 -0500

    name in test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -93,8 +93,8 @@ define([
         console.log("payloadAntes1--> con npmbre",payload['arguments'].execute.inArguments[0]);
         console.log("payloadAntes2--> con npmbre",payload['arguments'].execute.inArguments[1]);
         // console.log("payloadAntes3--> con npmbre",payload['arguments'].execute.inArguments[3].phone);
-        console.log("payloadAntes4--> con celular",payload['arguments'].execute.inArguments[0].Phone);
-        console.log("payloadAntes4--> con nombre",payload['arguments'].execute.inArguments[1].Name);
+        console.log("payloadAntes4--> con celular",payload['arguments'].execute.inArguments[0].name);
+        console.log("payloadAntes4--> con nombre",payload['arguments'].execute.inArguments[1].phone);
 
         // console.log("payloadAntes--> con Nombre",payload['arguments'].execute.inArguments[1].phone);
 
