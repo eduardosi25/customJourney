6ce1de35751eac63ea642b6b701f1697e759029a
eduardosi25 <eduardosi25@hotmail.com>
2022-06-20 15:35:35 -0500

    name in test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -93,7 +93,7 @@ define([
         console.log("payloadAntes1--> con it1",payload['arguments'].execute.inArguments[0]);
         // console.log("payloadAntes3--> con npmbre",payload['arguments'].execute.inArguments[3].phone);
 
-        console.log("payloadAntes4--> con nombre",payload['arguments'].execute.inArguments[0].phone);
+        // console.log("payloadAntes4--> con nombre",payload['arguments'].execute.inArguments[0].phone);
 
         // console.log("payloadAntes--> con Nombre",payload['arguments'].execute.inArguments[1].phone);
 
