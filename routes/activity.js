c4048939608a0fb64035ea1457e83648f0f958b6
eduardosi25 <eduardosi25@hotmail.com>
2022-06-20 17:04:38 -0500

    name in test

--- routes/activity.js
+++ routes/activity.js
@@ -83,10 +83,10 @@ JWT(req.body, process.env.jwtSecret, (err, decoded) => {
         }
         console.log("decoded",decoded.keyValue)
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-                var inArguments = decoded.inArguments;
-                console.log("inargumentsaxios-->",inArguments[0].keyValue)
+                var inArguments = decoded.keyValue;
+                console.log("inargumentsaxios-->",inArguments)
                 // logData(req);
-                var stringData = {"phone": inArguments[0].keyValue}
+                var stringData = {"phone": inArguments}
                 // console.log("stringData----->",stringData)
                 axios.defaults.headers = {
                 'Content-Type': 'application/json'
